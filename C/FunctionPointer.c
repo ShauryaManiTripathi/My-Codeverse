8474ead3e3a187c735628d23bcd357f12ab56a6e
Shaurya Manit Tripathi <shaurya.deoria@gmail.com>
2023-07-03 19:39:31 +0530

    commit

---
+++ C/FunctionPointer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef int (*f)(int a,int b);
+int add(int a, int b){
+return a+b;
+}
+int sub(int a, int b){
+return a-b;
+}
+f fp(f test){
+return test==add?sub:add;
+}
+int main(){
+    int (*g)(int a,int b)=sub;
+    printf("%d",fp(sub)(1,2));
+}
+
