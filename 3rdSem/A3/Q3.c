9573ac6b414d37e3046b7fe73df6fccb3df41310
shaurya <shaurya.deoria@gmail.com>
2023-08-19 16:04:37 +0000

    cummit

--- 3rdSem/A3/Q3.c
+++ 3rdSem/A3/Q3.c
@@ -19,18 +19,21 @@ int findMaxProd(int *a,int size){
         int size1=size/2;
         int cand1=findMaxProd(a,size1);
         int cand2=findMaxProd(a+size1,size-size1);
+        //////////////////
         int max1=INT_MIN,min1=INT_MAX,cur1=1;
         for(int i=size1-1;i>=0;i--){
             cur1*=a[i];
             if(cur1>max1)max1=cur1;
             if(cur1<min1)min1=cur1;
         }
+        ///////////////////
         int max2=INT_MIN,min2=INT_MAX,cur2=1;
         for(int i=size1;i<size;i++){
             cur2*=a[i];
             if(cur2>max2)max2=cur2;
             if(cur2<min2)min2=cur2;
         }
+        ///////////////////
         return Max(cand1,cand2,max1*max2,min1*min2);
     }
 }
