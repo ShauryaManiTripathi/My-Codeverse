21df0392515480beafce978799a999f060472442
shaurya <shaurya.deoria@gmail.com>
2023-08-20 15:50:03 +0000

    commit

---
+++ Cpp/Strings/SanketAndStrings.cpp
@@ -0,0 +1,18 @@
+// Sanket has a string consisting of only 'a' and 'b' as the characters. Sanket describes perfectness of a string as the maximum length substring of equal characters. Sanket is given a number **k** which denotes the maximum number of characters he can change. Find the maximum perfectness he can generate by changing no more than **k** characters.
+// Input Format: The first line contains an integer denoting the value of K.
+// The next line contains a string having only ‘a’ and ‘b’ as the characters.
+// Sample Input: 2
+// abba
+// Output Format: A single integer denoting the maximum perfectness achievable.
+// Sample Output: 4
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    cout<<s;
+    getline(stdin,s,'\n');
+    
+}
